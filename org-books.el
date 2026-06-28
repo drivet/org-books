9b5d256c9fdb241ffaac95699c63551a2b49c3b6
Desmond Rivet <desmond.rivet@gmail.com>
2026-06-28 16:54:27 -0400

    Get rid of helm and simplfy functions

--- org-books.el
+++ org-books.el
@@ -34,8 +34,6 @@
 (require 'dash)
 (require 'enlive)
 (require 'json)
-(require 'helm)
-(require 'helm-org)
 (require 'org)
 (require 's)
 (require 'subr-x)
@@ -45,21 +43,6 @@
 (defgroup org-books nil
   "Org reading list management."
   :group 'org)
-
-(defcustom org-books-file nil
-  "File for keeping reading list."
-  :type 'file
-  :group 'org-books)
-
-(defcustom org-books-add-to-top t
-  "Should add new books as the first item under a heading?"
-  :type 'boolean
-  :group 'org-books)
-
-(defcustom org-books-file-depth 2
-  "The max depth for adding book under headings."
-  :type 'integer
-  :group 'org-books)
 
 (defcustom org-books-url-pattern-dispatches
   '(("^\\(www\\.\\)?amazon\\." . org-books-get-details-amazon)
@@ -95,17 +78,16 @@ PAGE-NODE is the return value of `enlive-fetch' on the page url."
          (title (org-books--clean-str (enlive-text (enlive-get-element-by-id page-node "productTitle"))))
          (author (s-join ", " (org-books-get-details-amazon-authors page-node))))
     (if (not (string-equal title ""))
-        (list title author `()
-              (org-books-create-amazon-content url) url))))
+        (list title author `() "" url))))
 
 (defun org-books-get-details-goodreads (url)
   "Get book details from Goodreads URL."
   (let* ((page-node (enlive-fetch url))
-         (title (org-books--clean-str (enlive-text (enlive-query page-node [.Text__title1]))))
-         (author (org-books--clean-str (s-join ", " (mapcar #'enlive-text (enlive-query-all page-node [.ContributorLink__name] ))))))
+          (title (org-books--clean-str (enlive-text (enlive-query page-node [.Text__title1]))))
+          (author (org-books--clean-str
+                    (s-join ", " (mapcar #'enlive-text (enlive-query-all page-node [.ContributorLink__name] ))))))
     (if (not (string-equal title ""))
-        (list title author `()
-             (org-books-create-goodreads-content url) url))))
+        (list title author `() "" url))))
 
 (defun org-books-get-url-from-isbn (isbn)
   "Make and return openlibrary url from ISBN."
@@ -118,12 +100,8 @@ PAGE-NODE is the return value of `enlive-fetch' on the page url."
   (format "#+BEGIN_aside\n#+ATTR_HTML: :loading lazy\n[[%s]]\n#+END_aside"
           (create-isbn-cover-image-url isbn)))
 
-(defun org-books-create-amazon-content (url) "")
-
-(defun org-books-create-goodreads-content (url) "")
-
 (defun org-books-get-details-isbn (url)
-  "Get book details from openlibrary ISBN response from URL."
+  "Get book details from openlibrary ISBN response from (JSON) URL."
   (message "ISBN %s" url)
   (let* ((json-object-type 'hash-table)
          (json-array-type 'list)
@@ -139,8 +117,7 @@ PAGE-NODE is the return value of `enlive-fetch' on the page url."
           (org-books-create-isbn-content rawisbn) pageurl)))
 
 (defun org-books-get-details-olb (url)
-  "Get book details freom Open Library book URL"
-  (message "OLB")
+  "Get book details from Open Library book URL"
   (let* ((match-index (string-match "\\(https://openlibrary\\.org/books/[[:alnum:]]+\\)/" url))
          (m (match-string 1 url))
          (jsonurl (format "%s.json" m))
@@ -155,7 +132,7 @@ PAGE-NODE is the return value of `enlive-fetch' on the page url."
   "Fetch book details from given URL.
 
 Return a list of three items: title (string), author (string) and
-an alist of properties to be applied to the org entry. If the url
+an alist of properties to be applied to the org entry.  If the url
 is not supported, throw an error."
   (let* ((output 'no-match)
           (url-struct (url-generic-parse-url url))
@@ -173,78 +150,33 @@ is not supported, throw an error."
           (error (format "Url %s not understood" url))
           output)))))
 
-(defun org-books-create-file (file-path)
-  "Write initialization stuff in a new file at FILE-PATH."
-  (interactive "FFile: ")
-  (if (file-exists-p file-path)
-      (message "There is already a file present, skipping.")
-    (with-temp-file file-path
-      (insert "#+TITLE: Reading List\n"
-              "#+AUTHOR: " (replace-regexp-in-string "" " " user-full-name) "\n\n"
-              "#+TODO: READING NEXT | READ\n\n"))))
+(defun org-books-format (title author &optional props content url)
+  "Return formatted details as an org headline entry.
 
-(defun org-books-all-authors ()
-  "Return a list of authors in the `org-books-file'."
-  (with-current-buffer (find-file-noselect org-books-file)
-    (->> (org-property-values "Author")
-       (-reduce-from (lambda (acc line) (append acc (s-split "," line))) nil)
-       (mapcar #'s-trim)
-       (-distinct)
-       (-sort #'s-less-p))))
+If both TITLE and URL, the headline is a link.
+If only TITLE is defined, the headline is just the TITLE.
+AUTHOR and properties from PROPS go as org-property.  CONTENT is
+the actual content below"
+  (with-temp-buffer
+    (org-mode)
+    (if (null url)
+      (insert "* " title "\n")
+      (let ((headline (format "[[%s][%s]]" url title)))
+        (insert "* " headline "\n")))
+    (org-set-property "Author" author)
+    (org-set-property "Added" (format-time-string "%Y-%02m-%02d"))
+    (dolist (prop props)
+      (org-set-property (car prop) (cdr prop)))
+    (if content
+      (insert "\n" content "\n"))
+    (buffer-substring-no-properties (point-min) (point-max))))
 
-(defun org-books-entry-p ()
-  "Tell if current entry is an org-books entry."
-  (if (org-entry-get nil "Author") t))
+(defun org-books--insert (title author &optional props content url)
+  "Insert book template at current position in buffer.
 
-(defun org-books-get-closed-time ()
-  "Return closed time of the current entry."
-  (let ((ent-body (buffer-substring-no-properties (org-entry-beginning-position) (org-entry-end-position))))
-    (if (string-match org-closed-time-regexp ent-body)
-        (parse-time-string (match-string-no-properties 1 ent-body)))))
-
-(defun org-books-map-entries (func &optional match scope &rest skip)
-  "Similar to `org-map-entries' but only walks on org-books entries.
-
-Arguments FUNC, MATCH, SCOPE and SKIP follow their definitions
-from `org-map-entries'."
-  (with-current-buffer (find-file-noselect org-books-file)
-    (let ((ignore-sym (gensym)))
-      (-remove-item ignore-sym
-                    (apply #'org-map-entries
-                           (lambda ()
-                             (if (org-books-entry-p)
-                                 (if (functionp func) (funcall func) (funcall (list 'lambda () func)))
-                               ignore-sym))
-                           match scope skip)))))
-
-(defun org-books--get-active-books (&optional todo-keyword)
-  "Return books that are currently active. Each item returned is
-a pair of book name and position of the headline. Activity is
-assumed, by default, to be marked by READING TODO state."
-  (let ((active-todo-keyword "READING"))
-    (org-books-map-entries
-     (lambda ()
-       (cons
-        (substring-no-properties (org-get-heading) (+ 1 (length (or todo-keyword active-todo-keyword))))
-        (point)))
-     (format "TODO=\"%s\"" (or todo-keyword active-todo-keyword)))))
-
-(defun org-books-visit-book-log ()
-  "Ask to pick a book from currently active one and position
-cursor to add log entry."
-  (let ((active-books (org-books--get-active-books)))
-    (if (null active-books)
-        (message "No books active at the moment.")
-      (let ((picked-book
-             (helm :sources (helm-build-sync-source "Active books"
-                              :candidates active-books)
-                   :buffer "*helm active books*")))
-        (find-file org-books-file)
-        (goto-char picked-book)
-        (unless (re-search-forward "^*+ Log$" nil t)
-          (org-insert-heading-after-current)
-          (org-do-demote)
-          (insert "Log\n"))))))
+Formatting is specified by TITLE, AUTHOR, PROPS and CONTENT as
+described in docstring of `org-books-format' function."
+  (insert (org-books-format title author props content url)))
 
 ;;;###autoload
 (defun org-books-cliplink ()
@@ -254,102 +186,19 @@ cursor to add log entry."
     (org-books-add-url url)))
 
 ;;;###autoload
+(defun org-books-add-isbn (isbn)
+  "Add book from ISBN."
+  (interactive "sISBN: ")
+  (org-books-add-url (org-books-get-url-from-isbn isbn)))
+
+;;;###autoload
 (defun org-books-add-url (url)
   "Add book from web URL."
   (interactive "sUrl: ")
   (let ((details (org-books-get-details url)))
     (if (null details)
         (message "Error in fetching url. Please retry.")
-      (apply #'org-books-add-book details))))
-
-;;;###autoload
-(defun org-books-add-isbn (isbn)
-  "Add book from ISBN."
-  (interactive "sISBN: ")
-  (org-books-add-url (org-books-get-url-from-isbn isbn)))
-
-(defun org-books-format (level title author &optional props url)
-  "Return details as an org headline entry.
-
-LEVEL specifies the headline level. TITLE goes as the main text.
-AUTHOR and properties from PROPS go as org-property.  CONTENT is
-the actual content"
-  (with-temp-buffer
-    (org-mode)
-    (if (null url)
-      (insert (make-string level ?*) " " title "\n")
-      (let ((headline (format "[[%s][%s]]" url title)))
-        (insert (make-string level ?*) " " headline "\n")))
-    (org-set-property "Author" author)
-    (org-set-property "Added" (format-time-string "%Y-%02m-%02d"))
-    (dolist (prop props)
-      (org-set-property (car prop) (cdr prop)))
-    (buffer-substring-no-properties (point-min) (point-max))))
-
-(defun org-books--insert (level title author &optional props content url)
-  "Insert book template at current position in buffer.
-
-Formatting is specified by LEVEL, TITLE, AUTHOR, PROPS and CONTENT as
-described in docstring of `org-books-format' function."
-  (insert (org-books-format level title author props url))
-  (insert "\n" content "\n"))
-
-(defun org-books--insert-at-pos (pos title author &optional props content url)
-  "Goto POS in current buffer, insert a new entry and save buffer.
-
-TITLE, AUTHOR, PROPS AND CONTENT are formatted using `org-books-format'."
-  (org-content)
-  (goto-char pos)
-  (let ((level (or (org-current-level) 0)))
-    (org-books-goto-place)
-    (insert "\n")
-    (org-books--insert (+ level 1) title author props content url)
-    (save-buffer)))
-
-(defun org-books-goto-place ()
-  "Move to the position where insertion should happen."
-  (if org-books-add-to-top
-      (let ((level (or (org-current-level) 0))
-            (bound (save-excursion (org-get-next-sibling))))
-        (if (re-search-forward (format "^\\*\\{%s\\}" (+ level 1)) bound t)
-            (forward-line -1)))
-    (if (org-get-next-sibling)
-        (forward-line -1)))
-  (goto-char (line-end-position)))
-
-(defun org-books-get-headers ()
-  "Return list of categories under which books can be filed.
-
-Each item in list is a pair of title (propertized) and marker
-specifying the position in the file."
-  (let ((helm-org-headings-max-depth org-books-file-depth))
-    (mapcar (lambda (it)
-              (cons it (get-text-property 0 'helm-realvalue it)))
-            (helm-org--get-candidates-in-file org-books-file helm-org-headings-fontify t nil t))))
-
-;;;###autoload
-(defun org-books-add-book (title author &optional props content url)
-  "Add a book (specified by TITLE and AUTHOR) to the `org-books-file'.
-
-Optionally apply PROPS and add CONTENT"
-  (interactive
-   (let ((completion-ignore-case t))
-     (list
-      (read-string "Book Title: ")
-      (s-join ", " (completing-read-multiple "Author(s): " (org-books-all-authors))))))
-  (if org-books-file
-      (save-excursion
-        (with-current-buffer (find-file-noselect org-books-file)
-          (let ((headers (org-books-get-headers)))
-            (if headers
-                (helm :sources (helm-build-sync-source "org-book categories"
-                                 :candidates (mapcar (lambda (h) (cons (car h) (marker-position (cdr h)))) headers)
-                                 :action (lambda (pos) (org-books--insert-at-pos pos title author props content)))
-                      :buffer "*helm org-books add*")
-              (goto-char (point-max))
-              (org-books--insert 1 title author props content url)
-              (save-buffer)))))
-    (message "org-books-file not set")))
+      (apply #'org-books--insert details))))
 
 ;;;###autoload
 (defun org-books-rate-book (rating)
